db624401ade766b4fb5f912d79f338acdeac1016
tmathmeyer <tmathmeyer@gmail.com>
2013-05-13 13:51:48 -0400

    removed everything for a redo

--- src/edu/wpi/tmathmeyer/security/CryptoInformation.java
+++
@@ -1,7 +0,0 @@
-package edu.wpi.tmathmeyer.security;
-
-import edu.wpi.tmathmeyer.protocol.Packet;
-
-public class CryptoInformation {
-	public static Packet[] pkts = {new AuthPacket(new byte[0], "", (byte) 0),  new InformationPacket("")};
-}
